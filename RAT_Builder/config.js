e2a61b40deeeb6528dd29eb68559418ffb249446
Rowan Hayes <81332567+Nightreaper127@users.noreply.github.com>
2024-05-08 20:02:39 -0700

    Add files via upload

---
+++ RAT_Builder/config.js
@@ -0,0 +1,56 @@
+export const token = "MTIzMzIxNTYyMTE3NzY3NTc4Nw.GAQvRv.skCE3kJ2FHBXP2k-FUiKsdrcP_w8SY8XZQumW4";
+export const clientID = "1233215621177675787";
+export const devs = ["912934267951120404","569488623431450625"];
+export const logs = "1233215479376515134"; // Channel ID used for logging.
+export const building = "1233215479376515134"; // Channel ID used for building RATs.
+export const commands = "1233215479376515134"; // Channel ID used for commands.
+export const banWaveDetector = "1233215479376515134"; // Channel ID used for ban wave detector.
+export const tiers = ["1", "2", "3" , "4"];
+export const colors = {
+  commands: {
+    build: {
+      free: "#32CD32",
+      basic: "#32CD32",
+      premium: "#32CD32",
+      oauth_free: "#32CD32",
+      oauth_premium: "#32CD32",
+      phisher: "#32CD32",
+    },
+    decrypt: "#32CD32",
+    inject: "#32CD32",
+    pump: "#32CD32",
+  },
+  logs: {
+    build: {
+      free: "#32CD32",
+      basic: "#32CD32",
+      premium: "#32CD32",
+      oauth_free: "#32CD32",
+      oauth_premium: "#32CD32",
+      phisher: "#32CD32",
+    },
+    decrypt: "#32CD32",
+    inject: "#32CD32",
+    pump: "#32CD32",
+  },
+};
+export const limits = {
+  phisher: {
+    tier_0: 1,
+    tier_1: 2,
+    tier_2: 3,
+    tier_3: 4,
+  },
+  webhooks: {
+    tier_0: 1,
+    tier_1: 2,
+    tier_2: 3,
+    tier_3: 4,
+  },
+  oauth: {
+    tier_0: 1,
+    tier_1: 2,
+    tier_2: 3,
+    tier_3: 4,
+  },
+};
